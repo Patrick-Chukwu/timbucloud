99705bbdb417ae49df9a875f6fec1aa6cca2610b
Chukwu Patrick Ifeanyi <chukwupatrickify@gmail.com>
2024-07-05 20:31:31 +0100

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,9 +7,9 @@ const Home = () => {
   return (
     <div>
       <Navbar/>
-      <header className="w-[95%] max-w-[1280px] mx-auto   bg-cover bg-center rounded-3xl " style={{backgroundImage: "url('/hero.png')"}}>
-      <div className="p-8 h-[400px] gradient w-full rounded-3xl  my-4  flex flex-col items-start justify-end gap-2 ">
-      <motion.h1 whileInView={{scale:1}} initial={{scale:.8}} className='pt-24 text-4xl md:text-5xl font-black text-background opacity-90'>Autumn Collection</motion.h1>
+      <header className="w-[95%] max-w-[1280px] mx-auto   bg-cover bg-center rounded-3xl " style={{backgroundImage: "url('/hero.webp')"}}>
+      <div className="p-8 h-[321px] md:h-[396px] lg:h-[400px] gradient w-full rounded-3xl  my-4  flex flex-col items-start justify-end gap-2 ">
+      <motion.h1 whileInView={{scale:1}} initial={{scale:.8}} className='pt-16 text-4xl md:text-5xl font-black text-background opacity-90'>Autumn Collection</motion.h1>
       <div className="flex flex-col gap-2 justify-start items-start md:flex-row md:justify-between md:items-center md:w-[95%]">
         <p className="text-[16px] md:text-xl font-normal text-background">Get the latest tech products at CRAZY discount prices.</p>
         <Button className="text-xl font-bold">Buy Now</Button>

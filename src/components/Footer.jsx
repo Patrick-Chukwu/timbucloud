0976e22e9c1709b63b3f8a913da2603a85010a3e
Chukwu Patrick Ifeanyi <chukwupatrickify@gmail.com>
2024-07-06 13:01:26 +0100

    Complete home

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,23 +1,32 @@
 import React from 'react'
 import logo from '/logo.svg'
-// import { InstagramIcon } from 'react-icons'
-// import { TwitterIcon } from 'react-icons'
+import fb from '../assets/fb.png'
+import x from '../assets/x.png'
+import ig from '../assets/ig.png'
+
+
 const Footer = () => {
   return (
-    <div className='bg-primary w-full'>
+    <div className='bg-primary w-full py-4'>
       <div className="mx-auto w-[90%]">
-      <div className="flex flex-col gap-4 py-4 w-full border-b border-secondary">
+      <div className="flex flex-col gap-4 py-4 w-full border-b">
     <img src={logo} alt="Timbucloud logo" className='w-[335px] md:w-[622px]'/>
     <p className="text-xl font-normal text-foreground md:w-[624px]">Your one-stop tech shop. The biggest personal computing retailer in West Africa</p>
     </div>
-    <div className="flex justify-between items-center font-normal text-sm text-foreground">
-      <div className="flex gap-2 items-center">
-        {/* <TwitterIcon/>
-        <InstagramIcon/> */}
+    <div className="flex flex-col md:flex-row items-start justify-between md:items-center font-normal text-sm text-foreground my-4 gap-4">
+      <div className="flex gap-2 items-center text-foreground ">
+        <img src={fb} alt="Facebook Icon" />
+        <img src={x} alt="X Icon" />
+        <img src={ig} alt="Instagram Icon" />
+
         
       </div>
-      <div className="flex flex-col items-start gap-2">
+      <div className="flex flex-col items-start md:flex-row md:items-center">
         <p className='hover:bg-foreground hover:text-background p-1'>Terms of Service</p>
+                <p className='hover:bg-foreground hover:text-background p-1'>Privacy Policy</p>
+                <p className='hover:bg-foreground hover:text-background p-1'>Returns & Refunds</p>
+
+        
       </div>
     </div>
 

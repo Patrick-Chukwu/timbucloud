f532bab37ad75b60d7cdc3dd0b5750642c0372c0
Chukwu Patrick Ifeanyi <chukwupatrickify@gmail.com>
2024-07-06 09:47:36 +0100

    Reduce gradient's opacity

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -3,7 +3,7 @@ import { Button } from './ui/button';
 
 const Product = ({ product, onClick }) => {
   return (
-    <div className="flex flex-col gap-2 items-start justify-center bg-background shadow-lg rounded-[14px] xl:rounded-[20px] p-4 h-[428px] w-[335px] md:w-[312px] xl:w-[400px]" onClick={() => onClick(product)}>
+    <div className="flex flex-col gap-2 items-start justify-center bg-background shadow-lg rounded-[14px] xl:rounded-[20px] px-4 py-8  h-[428px] w-[335px] md:w-[312px] xl:w-[400px]" onClick={() => onClick(product)}>
       <img src={product.image1} alt={product.name} className="w-[311px] md:w-[288px] xl:w-[376px] h-[250px] object-contain rounded-3xl" />
       <h2 className="text-xl font-bold text-foreground w-full">{product.name}</h2>
       <p className="text-sm font-extrabold">{product.tag}</p>

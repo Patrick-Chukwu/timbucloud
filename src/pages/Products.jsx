47d64576d77ca5dd7d31498ad1779f730205fe99
Chukwu Patrick Ifeanyi <chukwupatrickify@gmail.com>
2024-07-16 15:01:11 +0100

    feat: import api

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,13 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
+const apiKey = import.meta.env.VITE_API_KEY;
+const orgId = import.meta.env.VITE_ORG_ID;
+const appId = import.meta.env.VITE_APP_ID;
+
 const Products = () => {
     const [products, setProducts] = useState([]);
     const [loading, setLoading] = useState(true);
 
     const fetchData = async () => {
         try {
-            const response = await axios.get("api/products?organization_id=d36c045231bb47b98344174449db4206&reverse_sort=false&page=1&size=10&Appid=YK1X1ZCXEX2FWDG&Apikey=79e32a1dd49c404e811a0ae6c836f1f420240712175938031711");
+            const response = await axios.get(`api/products?organization_id=${orgId}&reverse_sort=false&page=1&size=10&Appid=${appId}&Apikey=${apiKey}`);
             console.log(response.data.items);
             setProducts(response.data.items);
         } catch (error) {
@@ -30,7 +34,8 @@ const Products = () => {
                 <div className="flex flex-wrap gap-4 max-w-[98%] mx-auto px-4">
                     {products.map(product => (
                         <div key={product.unique_id} className="flex flex-col gap-4 border p-4 w-[300px] mx-auto">
-                     <h2 className='text-4xl text-left text-foreground font-bold'>{product.name}</h2>
+                     <h2 className='text-xl font-bold text-foreground w-full text-left'>{product.name}</h2>
+                     <p className="text-sm font-extrabold text-destructive">{product.description}</p>
                             {product.photos && product.photos.length > 0 && (
                          <img 
                                     src={`https://api.timbu.cloud/${product.photos[0].url}`} 
@@ -44,6 +49,7 @@ const Products = () => {
                                     {`https://api.timbu.cloud/${product.photos[0].url}`}
                                 </p>
                             )}
+                              <p className="text-foreground text-2xl font-bold">{product.price}</p>
                         </div>
                     ))}
                 </div>
